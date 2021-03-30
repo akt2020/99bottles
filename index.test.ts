4309120198ed15cd295415e3461a1fad3899ddf8
Ankit Tater <ankittater91@gmail.com>
2021-03-31 00:29:10 +0530

    Program to print 99 bee lyrics with unit test

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,47 @@
-import add from "./index";
-describe("testing", () => {
-  it("should return 15 for add(10+5)", () => {
-    expect(add(10, 5)).toBe(15);
+import { BeerLyrics } from "./index";
+
+import { lyrics } from "./lyrics";
+
+let beerLyrics = new BeerLyrics();
+
+describe("BeerLyrics", () => {
+  describe("getBottleText", () => {
+    it("should return singular text if the number is 1", () => {
+      expect(beerLyrics.getBottleText(1)).toBe("1 bottle");
+    });
+
+    it("should return `no more bottles` if the number is 0", () => {
+      expect(beerLyrics.getBottleText(0)).toBe("no more bottles");
+    });
+
+    it("should return plural text if the number is more then 1", () => {
+      expect(beerLyrics.getBottleText(2)).toBe("2 bottles");
+    });
+  });
+
+  describe("getLyricsParagraph", () => {
+    it("should return lyrics with bottle if the number is 1", () => {
+      expect(beerLyrics.getLyricsParagraph(1)).toBe(
+        `1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n`
+      );
+    });
+
+    it("should return lyrics with bottles if the number is 2", () => {
+      expect(beerLyrics.getLyricsParagraph(2)).toBe(
+        `2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n`
+      );
+    });
+
+    it("should return lyrics with bottles if the number is 3", () => {
+      expect(beerLyrics.getLyricsParagraph(3)).toBe(
+        `3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n`
+      );
+    });
+  });
+
+  describe("getBeerSongLyrics", () => {
+    it("should return the complete lyrics", () => {
+      expect(beerLyrics.getBeerSongLyrics(99)).toBe(lyrics);
+    });
   });
 });
--- index.ts
+++ index.ts
@@ -1,5 +1,29 @@
-export default function add(a: number, b: number): number {
-  return a + b;
+class BeerLyrics {
+  getBottleText(count: number): string {
+    return `${count ? count : "no more"} bottle${count !== 1 ? "s" : ""}`;
+  }
+
+  getLyricsParagraph(count: number): string {
+    return `${this.getBottleText(
+      count
+    )} of beer on the wall, ${this.getBottleText(
+      count
+    )} of beer.\nTake one down and pass it around, ${this.getBottleText(
+      --count
+    )} of beer on the wall.\n`;
+  }
+
+  getBeerSongLyrics(count: number): string {
+    let tempCount: number = count;
+    let result: string = "";
+    while (tempCount > 0) {
+      result += this.getLyricsParagraph(tempCount) + "\n";
+      tempCount--;
+    }
+
+    result += `No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, ${count} bottles of beer on the wall.`;
+    return result;
+  }
 }
 
-console.log(add(10, 5));
+export { BeerLyrics };
